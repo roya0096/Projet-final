15a4edc2f48397ac12d8dc8c61db053deaace990
roya0096 <131020995+roya0096@users.noreply.github.com>
2023-04-21 10:06:05 -0400

    Update projet.py

--- projet.py
+++ projet.py
@@ -7,84 +7,118 @@ import pandas
 col1, col2= st.columns(2)
 
 with col1:
-    st.title("Calcul financier")
+    st.title("Calculatrice pour fonction financière")
 with col2:
     image = Image.open('image_finance.jpg')
 
     st.image(image, caption='')
 
 
-choix =  st.sidebar.selectbox ("Quel type de calcul financier désirez-vous faire ?",( 'Obligation','Options', 'Valeurs', 'Ratio' ))
+choix =  st.sidebar.selectbox ("Quel type de calcul financier désirez-vous faire ?",( 'Obligation','Options', 'Ratio', 'Valeurs intrisèque' ))
 
 
 
-if choix == 'Options' :
-    st.title(" Voici la section pour les calculs sur les options")
-
-    prix_s = st.number_input(" Quelle est le prix de l'option sous-jacent? ")
-    prix_k = st.number_input(" Quelle est le prix d'exercise de cette option?")
-    temp = st.number_input(" Quelle est l'échéance de cette option ?")
-    taux = st.number_input(" Quelle est le taux sans risque ?")
-    sig = st.number_input(" Quelle est l'écart type du rendement de l'action sous-jacent?")
+if choix == 'Options' : # Cette section demande les informations pour être en mesure de faire les calculs sur les options
+    st.header("Voici la section pour les calculs sur les options")
+    st.subheader("Veuiller entrer les informations suivantes")
+   
+    prix_s = st.number_input("Quelle est le prix de l'action sous-jacent? ", value=1.00)
+    prix_k = st.number_input("Quelle est le prix d'exercise de cette option?",value=1.00)
+    temp = st.number_input("Quelle est l'échéance de cette option ?",value=1.00)
+    taux = st.number_input("Quelle est le taux sans risque ?",value=1.00)
+    sig = st.number_input("Quelle est l'écart type du rendement de l'action sous-jacent ?",value=1.00)
     
+    #On vient créer notre liste pour être en mesure de prendre faire le lien avec notre page formule
     option_1 = fm.Options(S = prix_s, X = prix_k, T = temp, r = taux, sigma = sig)
     call = option_1.bs_call()
     put = option_1.bs_put()
 
-    st.write(f"C'est la valeurs du call pour le denier point {call}.")
+    st.header("Voici vos résultats ")
+    st.write(f"Voici le prix du call selon les informations que vous avez entré : {call:.2f}$.")
+    st.write(f"Voici le prix du put selon les informations que vous avez entré : {put:.2f}$.")
     
 
 
 
-elif choix == 'Obligation':
-    st.title(" Voici la section pour les calculs sur les obligations")
-    dur = st.number_input("Quelle est la durée modifié de cette obligation ?")
-    delta_rend = st.number_input("Quelle est la variation du I de cette obligation?")
-    convex = st.number_input("Quel est la convexité de cette obligaton ?")
-    rend_exige = st.number_input(" Quel est le rendement exigé de cette obligation?")
-    coup = st.number_input(" Quel est le taux de coupon de cette obligation?")
-    temp = st.number_input(" Combien le nombre de période restante a cette obligations")
-    prix = st.number_input("Quelle est le prix présentement de l'obligation ")
+elif choix == 'Obligation': # Cette section demande les informations pour être en mesure de faire les calculs sur les obligations
+    st.header("Voici la section pour les calculs sur les obligations")
+    st.subheader("Veuiller entrer les informations suivantes")
 
+    dur = st.number_input("Quelle est la durée modifié de cette obligation ?",value=1.00)
+    delta_rend = st.number_input("Quelle est la variation du taux de cette obligation?",value=1.00)
+    convex = st.number_input("Quel est la convexité de cette obligaton ?",value=1.00)
+    rend_exige = st.number_input("Quel est le rendement de cette obligation ?",value=1.00)
+    coup = st.number_input(" Quel est le taux de coupon de cette obligation ?",value=1.00)
+    temp = st.number_input(" Combien reste t'il de période à cette obligation ?",value=1.00)
+    prix = st.number_input("Quelle est le prix présentement de l'obligation ? ",value=1.00)
+
+    #On vient créer notre liste pour être en mesure de prendre faire le lien avec notre page formule
     obligation_1 = fm.Obligation(d_mod = dur, conv = convex, delta_i = delta_rend, rend_exig = rend_exige, taux_coup = coup ,t = temp ,p = prix)    
-    var = obligation_1.variation_oblig
-    duree = obligation_1.duree
-    val = obligation_1.valeur
-    conv = obligation_1.convexite
+    var = obligation_1.variation_oblig()
+    dure = obligation_1.duree()
+    val = obligation_1.valeur()
+    conv = obligation_1.convexite()
 
-elif choix == 'Valeurs':
-    st.title(" Voici la section pour les calculs sur la valeurs d'une entreprise")
-    Rend_att = st.number_input("Quelle est le rendement attendu?")
-    rend_0 = st.number_input("Quelle est le taux sans risque")
-    flux_0 = st.number_input("Quelle est le dernier flux de l'entreprise ?")
-    bet = st.number_input(" Quel est le beta de cette entreprise")
-    croi = st.number_input(" Quel est le taux de croissance de l'entreprise")
-    cour_b = st.number_input(" Quelle est le cour/bénifice de cette entreprise")
-    cour_bx = st.number_input("Quelle est le prochain cour bénifice estimé de cette entreprise ")
-    beni_1= st.number_input("Quelle est le benifice par action +1 de cette entreprise")
-    beni_fice = st.number_input("Quelle est le bénifice de cette entreprise")
-    x=10
+    # On affiche les résultats sur notre site web 
+    st.header("Voici vos résultats ")
+    st.write(f"La variation de prix pour cette obligation est égal à {var:.2f}.")
+    st.write(f"La durée de cette obligation est {dure:.2f} années.")
+    st.write(f"La valeur de cette obligation est de {val:.2f}$.")
+    st.write(f"La convexité de cette obligation est de {conv:.2f}.")
 
+elif choix == 'Valeurs intrisèque': # Cette section demande les informations pour être en mesure de faire les calculs sur la valeur intrinsèque d'une entreprise
+    st.header(" Voici la section pour les calculs sur la valeurs d'une entreprise")
+    st.subheader("Veuiller entrer les informations suivantes")
+    
+    Rend_att = st.number_input("Quelle est le rendement attendu ?",value=1.00)
+    rend_0 = st.number_input("Quelle est le taux sans risque ?",value=1.00)
+    flux_0 = st.number_input("Quelle est le dernier flux monétaire de l'entreprise ?",value=1.00)
+    bet = st.number_input(" Quel est le beta de cette entreprise ?",value=1.00)
+    croi = st.number_input(" Quel est le taux de croissance de l'entreprise ?",value=1.00)
+    cour_b = st.number_input(" Quelle est le cour/bénifice de cette entreprise ?",value=1.00)
+    cour_bx = st.number_input("Quelle est le prochain cour/bénifice estimé de cette entreprise ?",value=1.00)
+    beni_1= st.number_input("Quelle est le benifice par action pour la prochaine période de cette entreprise ?",value=1.00)
+    beni_fice = st.number_input("Quelle est le bénifice de cette entreprise ?",value=1.00)
+    
+    x = []
+    for y in range (1,6):
+        flux = st.number_input(f"Quelle est le flux monétaire de l'année{y}?", value=1.00)
+        x.append(flux)
+
+
+    #On vient créer notre liste pour être en mesure de prendre faire le lien avec notre page formule
     valorisation = fm.Valeur(Rm = Rend_att, Rf = rend_0, fcf_0= flux_0, fcf_futurs=x,  beta = bet, g = croi, CB = cour_b, BPA1= beni_1, CBx = cour_bx, benifice = beni_fice)
-    CAPM = valorisation.capm
-    vale = valorisation.croissance_2
-    cb = valorisation.multiple_CB
+    CAPM = valorisation.capm()
+    vale = valorisation.croissance_2()
+    cb = valorisation.multiple_CB()
+    fin = CAPM * 100
 
-elif choix == 'Ratio':
-    st.title(" Voici la section sur des ratios financiers")
-    rend = st.number_input("Quelle est le rendement attendu?")
-    sig = st.number_input("Quelle est le sigma de ce portefeuille")
-    be = st.number_input("Quelle est le beta du portefeuille ")
-    rend_mar = st.number_input("Quelle est le rendement moyen du marché?")
-    rend_0r = st.number_input("Quelle est le taux sans risque?")
+    # On affiche les résultats sur notre site web 
+    st.header("Voici vos résultats ")
+    st.write(f"Le modèle d'évaluation des actifs pour cette entreprise est de {fin:.2f}%.")
+    st.write(f"La valeur de cette entreprise selon la méthode de cash flow est de {vale:.2f}$.")
+    st.write(f"La valeur de cette entreprise selon la méthode du cour bénifice est de {cb:.2f}$.")
 
+elif choix == 'Ratio': # Cette section demande les informations pour être en mesure de faire les calculs sur les ratios 
+    st.header(" Voici la section sur des ratios financiers")
+    st.subheader("Veuiller entrer les informations suivantes")
+    rend = st.number_input("Quelle est le rendement attendu?",value=1.00)
+    sig = st.number_input("Quelle est le sigma de ce portefeuille",value=1.00)
+    be = st.number_input("Quelle est le beta du portefeuille ",value=1.00)
+    rend_mar = st.number_input("Quelle est le rendement moyen du marché?",value=1.00)
+    rend_0r = st.number_input("Quelle est le taux sans risque?",value=1.00)
 
+    #On vient créer notre liste pour être en mesure de prendre faire le lien avec notre page formule
     rat = fm.Ratio(Rp =rend ,sigma = sig, beta = be, Rf =rend_mar, Rm= rend_0r)
+    shar = rat.sharpe()
+    trey = rat.treynor()
+    jen = rat.jenson()
 
-    shar = rat.sharpe
-    trey = rat.treynor
-    jen = rat.jenson
+    # On affiche les résultats sur notre site web 
+    st.header("Voici vos résultats ")
+    st.write(f" Le ratio Sharpe de votre portefeuille est de {shar:.2f}.")
+    st.write(f" Le ratio Treynor de votre portefeuille est de {trey:.2f}.")
+    st.write(f" Le ratio de Jenson de votre portefeuille est de {jen:.2f}.")
 
 
-# utiliser les append pour ajouter les formules dans une liste pour pouvoir prendre ensuite les êtres
-# en mesure d'utiliser les formules et les chiffres.
+st.write ("Ce site web fut créer par Anthony Roy, Antony Mercier-Saillant et Marc-Olivier Simard dans le but du cour Fin30521-71")
